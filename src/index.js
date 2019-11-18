d3c654dc6773a12fc54d7886c879343072715cc3
Eli Usupov <eliusupov123@gmail.com>
2019-11-18 17:58:07 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import Unit from './js/Classes/Unit/Unit';
 
 var canvas;
 var canvasContext;
-var ballX = 400;
-var ballY = 300;
+var toX;
+var toY;
 
-var speed = 5;
-var toX = ballX;
-var toY = ballY;
-var xSpeed = speed;
-var ySpeed = speed;
-
-const unit = new Unit();
+let selectedUnit;
+const unit = new Unit(200, 300);
+const unit2 = new Unit(400, 300);
 
 function calculateMousePos(evt) {
 	var rect = canvas.getBoundingClientRect(),
@@ -24,49 +20,61 @@ function calculateMousePos(evt) {
 	};
 }
 
-window.onload = function() {
-	canvas = document.getElementById('pongCanvas');
-	canvasContext = canvas.getContext('2d');
-	var framesPerSecond = 60;
-	setInterval(function() {
-		drawEverything(canvasContext);
-		unit.move(toX, toY, xSpeed, ySpeed);
-	}, 1000 / framesPerSecond);
+function calculateSpeed(unit, toX, toY) {
+	let targetToX;
+	let targetToY;
+	const unitX = unit.posX;
+	const unitY = unit.posY;
+	let xSpeed = unit.minSpeed;
+	let ySpeed = unit.minSpeed;
 
-	canvas.addEventListener('mousedown', function(evt) {
-		let targetToX;
-		let targetToY;
-		toX = evt.layerX;
-		toY = evt.layerY;
-		xSpeed = speed;
-		ySpeed = speed;
+	// let xSpeed = unit.xSpeed < unit.minSpeed ? unit.minSpeed : unit.xSpeed;
+	// let ySpeed = unit.ySpeed < unit.minSpeed ? unit.minSpeed : unit.ySpeed;
 
-		if (Math.abs(ballX) > Math.abs(toX)) {
-			targetToX = Math.abs(ballX) - Math.abs(toX);
-		} else {
-			targetToX = Math.abs(toX) - Math.abs(ballX);
-		}
+	if (Math.abs(unitX) > Math.abs(toX)) {
+		targetToX = Math.abs(unitX) - Math.abs(toX);
+	} else {
+		targetToX = Math.abs(toX) - Math.abs(unitX);
+	}
 
-		if (Math.abs(ballY) > Math.abs(toY)) {
-			targetToY = Math.abs(ballY) - Math.abs(toY);
-		} else {
-			targetToY = Math.abs(toY) - Math.abs(ballY);
-		}
+	if (Math.abs(unitY) > Math.abs(toY)) {
+		targetToY = Math.abs(unitY) - Math.abs(toY);
+	} else {
+		targetToY = Math.abs(toY) - Math.abs(unitY);
+	}
 
-		if (Math.abs(targetToX) > Math.abs(targetToY)) {
-			ySpeed = ySpeed / (targetToX / targetToY);
-		}
-		if (Math.abs(targetToX) < Math.abs(targetToY)) {
-			xSpeed = xSpeed / (targetToY / targetToX);
-		}
-	});
-};
+	if (Math.abs(targetToX) > Math.abs(targetToY)) {
+		ySpeed = ySpeed / (targetToX / targetToY);
+	}
+	if (Math.abs(targetToX) < Math.abs(targetToY)) {
+		xSpeed = xSpeed / (targetToY / targetToX);
+	}
+
+	return unit.setSpeed(xSpeed, ySpeed);
+}
 
 function drawEverything(canvasContext) {
 	// next line blanks out the screen with black
 	colorRect(0, 0, canvas.width, canvas.height, 'black');
-	unit.draw(canvasContext);
+	unit.initialise(canvasContext, toX, toY);
+	// unit2.initialise(canvasContext, toX, toY, xSpeed, ySpeed);
 }
+
+window.onload = function() {
+	canvas = document.getElementById('pongCanvas');
+	canvasContext = canvas.getContext('2d');
+	var framesPerSecond = 60;
+	setInterval(() => {
+		drawEverything(canvasContext);
+	}, 1000 / framesPerSecond);
+
+	canvas.addEventListener('mousedown', function(evt) {
+		// calculateSpeed(unit, evt.layerX, evt.layerY);
+		toX = evt.layerX;
+		toY = evt.layerY;
+		unit.calculateSpeed(toX, toY);
+	});
+};
 
 function colorCircle(centerX, centerY, radius, drawColor) {
 	canvasContext.fillStyle = drawColor;
--- src/js/Classes/Unit/Unit.js
+++ src/js/Classes/Unit/Unit.js
@@ -1,15 +1,38 @@
 class Unit {
+	constructor(posX, posY) {
+		this.setPos(posX, posY);
+	}
+
 	posX = 400;
 
 	posY = 300;
 
-	speed = 5;
+	xSpeed = 1;
+
+	ySpeed = 1;
 
 	maxSpeed = 10;
 
-	acceleration = 0.05;
+	acceleration = 0.1;
 
 	minSpeed = 0.1;
+
+	initialise = (canvasContext, toX, toY) => {
+		// if (this.xSpeed < this.maxSpeed) {
+		// 	this.setSpeed(this.xSpeed + this.acceleration, this.ySpeed)
+		// } else {
+		// 	this.setSpeed(this.maxSpeed, this.ySpeed)
+		// }
+		//
+		// if (this.ySpeed < this.maxSpeed) {
+		// 	this.setSpeed(this.xSpeed, this.ySpeed + this.acceleration)
+		// } else {
+		// 	this.setSpeed(this.xSpeed, this.maxSpeed)
+		// }
+		this.draw(canvasContext);
+		// this.calculateSpeed(toX, toY);
+		this.move(toX, toY, this.xSpeed, this.ySpeed);
+	};
 
 	draw = canvasContext => {
 		canvasContext.fillStyle = 'white';
@@ -19,6 +42,7 @@ class Unit {
 	};
 
 	move = (x, y, xSpeed, ySpeed) => {
+		if (!x || !y) return;
 		if (x === this.posX && y === this.posY) return;
 		const xMovingPlus = this.posX + xSpeed;
 		const xMovingMinus = this.posX - xSpeed;
@@ -55,20 +79,77 @@ class Unit {
 				unitY = yMovingMinus;
 			}
 		}
-		
-		this.setPos(unitX, unitY)
+		if (unitX !== x) {
+			this.xSpeed = this.xSpeed + this.acceleration;
+		}
+		// if (unitY !== y) {
+		// 	// this.setSpeed(this.xSpeed, this.ySpeed + this.acceleration)
+		// }
+		this.setPos(unitX, unitY);
 	};
+
+	calculateSpeed(toX, toY) {
+		let targetToX;
+		let targetToY;
+		const unitX = this.posX;
+		const unitY = this.posY;
+
+		let xSpeed = this.xSpeed;
+		let ySpeed = this.ySpeed;
+
+		// if (this.xSpeed < this.minSpeed) {
+		// 	xSpeed = this.minSpeed;
+		// } else if (this.xSpeed > this.maxSpeed) {
+		// 	xSpeed = this.maxSpeed;
+		// } else {
+		// 	xSpeed = this.xSpeed;
+		// }
+		//
+		// if (this.ySpeed < this.minSpeed) {
+		// 	ySpeed = this.minSpeed;
+		// } else if (this.ySpeed > this.maxSpeed) {
+		// 	ySpeed = this.maxSpeed;
+		// } else {
+		// 	ySpeed = this.ySpeed;
+		// }
+
+		if (Math.abs(unitX) > Math.abs(toX)) {
+			targetToX = Math.abs(unitX) - Math.abs(toX);
+		} else {
+			targetToX = Math.abs(toX) - Math.abs(unitX);
+		}
+
+		if (Math.abs(unitY) > Math.abs(toY)) {
+			targetToY = Math.abs(unitY) - Math.abs(toY);
+		} else {
+			targetToY = Math.abs(toY) - Math.abs(unitY);
+		}
+
+		if (Math.abs(targetToX) > Math.abs(targetToY)) {
+			ySpeed = ySpeed / (targetToX / targetToY);
+		}
+		if (Math.abs(targetToX) < Math.abs(targetToY)) {
+			xSpeed = xSpeed / (targetToY / targetToX);
+		}
+
+		return this.setSpeed(xSpeed, ySpeed);
+	}
 
 	setPos = (x, y) => {
 		this.posX = x;
 		this.posY = y;
 	};
-	
+
+	setSpeed = (x = this.xSpeed, y = this.ySpeed) => {
+		this.xSpeed = x;
+		this.ySpeed = y;
+	};
+
 	getPos = () => {
 		return {
 			x: this.posX,
 			y: this.posY,
-		}
+		};
 	};
 }
 
